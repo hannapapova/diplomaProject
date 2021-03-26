61080dabf7b380a0621a0903067c98928fc79891
Huckleberry <orpahdmitry@gmail.com>
2021-03-27 02:27:51 +0300

    fragment navigation added

--- app/src/main/java/com/example/weatherapplication/details/DetailsFragment.kt
+++ app/src/main/java/com/example/weatherapplication/details/DetailsFragment.kt
@@ -4,16 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.example.weatherapplication.R
+import com.example.weatherapplication.setupBar
+import com.example.weatherapplication.setupNavigation
 
 class DetailsFragment : Fragment() {
+    private val key = "DETAILS"
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        val view = inflater.inflate(R.layout.fragment_details, container, false)
+        val toolbar = activity?.findViewById<Toolbar>(R.id.toolbar)
+
+        setupBar(key, toolbar)
+        setupNavigation(key, view, toolbar)
+        return view
     }
 }
--- app/src/main/java/com/example/weatherapplication/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/weatherapplication/favorites/FavoritesFragment.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.weatherapplication.R
+import com.example.weatherapplication.setupBar
+import com.example.weatherapplication.setupNavigation
 
 class FavoritesFragment : Fragment() {
+    private val key = "FAVORITES"
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_favorites, container, false)
+        val view = inflater.inflate(R.layout.fragment_favorites, container, false)
+        val toolbar = activity?.findViewById<Toolbar>(R.id.toolbar)
+
+        setupBar(key, toolbar)
+        setupNavigation(key, view, toolbar)
+        return view
     }
 }
