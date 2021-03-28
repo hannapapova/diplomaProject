e56b779c50bd8710b84742b0f3cfa529b585cec9
Huckleberry322 <73405216+Huckleberry322@users.noreply.github.com>
2021-03-28 21:31:26 +0300

    Design (#3)
    * home fragment design +icons
    
    * settings design
    
    * app bar titles added

--- app/src/main/java/com/example/weatherapplication/home/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapplication/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -21,7 +22,9 @@ class HomeFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         val toolbar = activity?.findViewById<Toolbar>(R.id.toolbar)
+        val title = activity?.findViewById<TextView>(R.id.fragment_name)
 
+        title?.text = getString(R.string.home)
         setupBar(key, toolbar)
         setupNavigation(key, view, toolbar)
         return view
--- app/src/main/java/com/example/weatherapplication/search/SearchFragment.kt
+++ app/src/main/java/com/example/weatherapplication/search/SearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.example.weatherapplication.R
@@ -20,7 +21,9 @@ class SearchFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_search, container, false)
         val toolbar = activity?.findViewById<Toolbar>(R.id.toolbar)
+        val title = activity?.findViewById<TextView>(R.id.fragment_name)
 
+        title?.text = getString(R.string.search)
         setupBar(key, toolbar)
         setupNavigation(key, view, toolbar)
         return view
