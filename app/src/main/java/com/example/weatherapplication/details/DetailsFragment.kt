8ddc4673f41b77dc56414cbd9b30426ae9789dbb
Huckleberry <orpahdmitry@gmail.com>
2021-03-28 17:15:32 +0300

    app bar titles added

--- app/src/main/java/com/example/weatherapplication/details/DetailsFragment.kt
+++ app/src/main/java/com/example/weatherapplication/details/DetailsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import com.example.weatherapplication.R
@@ -20,7 +21,9 @@ class DetailsFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_details, container, false)
         val toolbar = activity?.findViewById<Toolbar>(R.id.toolbar)
+        val title = activity?.findViewById<TextView>(R.id.fragment_name)
 
+        title?.text = getString(R.string.details)
         setupBar(key, toolbar)
         setupNavigation(key, view, toolbar)
         return view
