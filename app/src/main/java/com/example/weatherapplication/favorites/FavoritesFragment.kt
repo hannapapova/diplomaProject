ad5720a8325b933b46b5db1d96ad2324e55e8046
Huckleberry <orpahdmitry@gmail.com>
2021-04-02 01:35:50 +0300

    added different weather bg \ rework setups

--- app/src/main/java/com/example/weatherapplication/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/weatherapplication/favorites/FavoritesFragment.kt
@@ -7,9 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import com.example.weatherapplication.R
+import com.example.weatherapplication.*
+import com.example.weatherapplication.setupBackgroundColor
 import com.example.weatherapplication.setupBar
-import com.example.weatherapplication.setupActionsBar
+import com.example.weatherapplication.setupBarActions
+import com.example.weatherapplication.setupTitle
 
 class FavoritesFragment : Fragment() {
     private val key = "FAVORITES"
@@ -23,9 +25,10 @@ class FavoritesFragment : Fragment() {
         val toolbar = activity?.findViewById<Toolbar>(R.id.toolbar)
         val title = activity?.findViewById<TextView>(R.id.fragment_name)
 
-        title?.text = getString(R.string.favorite)
+        setupTitle(title, key)
+        setupBackgroundColor(view)
         setupBar(key, toolbar)
-        setupActionsBar(key, view, toolbar)
+        setupBarActions(key, view, toolbar)
         return view
     }
 }
--- app/src/main/java/com/example/weatherapplication/home/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapplication/home/HomeFragment.kt
@@ -7,9 +7,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import com.example.weatherapplication.R
+import com.example.weatherapplication.*
+import com.example.weatherapplication.setupBarActions
 import com.example.weatherapplication.setupBar
-import com.example.weatherapplication.setupActionsBar
+import com.example.weatherapplication.setupTitle
 
 class HomeFragment : Fragment() {
     private val key = "HOME"
@@ -22,10 +23,15 @@ class HomeFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         val toolbar = activity?.findViewById<Toolbar>(R.id.toolbar)
         val title = activity?.findViewById<TextView>(R.id.fragment_name)
+        val window = activity?.window
 
-        title?.text = getString(R.string.home)
+
+        setupTitle(title, key)
+        setupToolBarBackgroundColor(toolbar, requireContext())
+        setupBackgroundColor(view)
+        setupStatusBarColor(window, requireContext())
         setupBar(key, toolbar)
-        setupActionsBar(key, view, toolbar)
+        setupBarActions(key, view, toolbar)
         return view
     }
 }
