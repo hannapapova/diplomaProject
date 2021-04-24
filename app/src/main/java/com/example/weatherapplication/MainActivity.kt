01c86f2e3b4f6ea57bd1b8238cb48dbc64cb6f1f
Huckleberry <orpahdmitry@gmail.com>
2021-04-24 22:42:38 +0300

    location changes

--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -1,150 +1,23 @@
 package com.example.weatherapplication
 
-import android.Manifest
 import android.app.Activity
-import android.app.Application
 import android.content.Context
-import android.content.SharedPreferences
-import android.content.pm.PackageManager
-import android.location.Geocoder
-import android.location.LocationManager
 import android.os.Bundle
-import android.os.Looper
-import android.util.Log
 import android.view.View
 import android.view.Window
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
-import com.example.weatherapplication.koin.WeatherViewModel
-import com.example.weatherapplication.room.entity.CurrentCity
-import com.google.android.gms.location.*
-import org.koin.android.ext.android.inject
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var locationRequest: LocationRequest
-    private lateinit var sharedPreferences: SharedPreferences
-    private lateinit var myApplication: Application
-    private var PERMISSION_ID = 322
-    private val viewModel by inject<WeatherViewModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_main)
-        myApplication = application
-        sharedPreferences = myApplication.getSharedPreferences("SHARED_PREFS", Context.MODE_PRIVATE)
-
-        if (sharedPreferences.getBoolean("LOCATION", true)) {
-            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-            getLastLocation()
-        }
-
-    }
-
-    private fun requestPermissions() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ), PERMISSION_ID
-        )
-    }
-
-    private fun isLocationServicesEnabled(): Boolean {
-        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-            LocationManager.NETWORK_PROVIDER
-        )
-    }
-
-    private fun getNewLocation() {
-        locationRequest = LocationRequest()
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationRequest.interval = 0
-        locationRequest.fastestInterval = 0
-        locationRequest.numUpdates = 2
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            fusedLocationClient.requestLocationUpdates(
-                locationRequest,
-                locationCallback,
-                Looper.myLooper()
-            )
-        }
-    }
-
-    private val locationCallback = object : LocationCallback() {
-        override fun onLocationResult(p0: LocationResult) {
-            viewModel.city = createCurrentCity(p0.lastLocation.latitude, p0.lastLocation.longitude)
-        }
-    }
-
-
-    private fun getLastLocation() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            if (isLocationServicesEnabled()) {
-                fusedLocationClient.lastLocation.addOnCompleteListener {
-                    val location = it.result
-                    if (location == null) {
-                        getNewLocation()
-                    } else {
-                        viewModel.city = createCurrentCity(it.result.latitude, it.result.longitude)
-                    }
-                }
-            } else {
-                Toast.makeText(this, "Please enable your location service", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        } else {
-            requestPermissions()
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == PERMISSION_ID) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Log.i("location", "Permission granted!")
-            }
-        }
-    }
-
-    private fun createCurrentCity(latitude: Double, longitude: Double): CurrentCity {
-        val geoCoder = Geocoder(applicationContext, Locale.ENGLISH)
-        val list = geoCoder.getFromLocation(latitude, longitude, 1)
-        return CurrentCity(
-            list[0].locality,
-            list[0].adminArea,
-            list[0].countryName,
-            latitude.toString(),
-            longitude.toString()
-        )
     }
 }
 
